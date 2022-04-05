471eea13f4883c2a5122b23f17aef526c9006c3b
Колобова Ксения Евгеньевна <shrimpksko@gmail.com>
2022-04-05 18:30:56 +0700

    add custom hook useRandomColor

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { inject, observer } from "mobx-react";
 import { rootStore } from "./root-store";
 import { Circle } from "./circle";
@@ -6,22 +6,44 @@ import { Logo } from "./logo";
 import './App.css';
 
 export const Main = inject('rootStore')(observer(({ rootStore } : any ) => {
+
+    const colors = ['#a0cddb', '#c4accf', '#A6D5BE', '#ddadaf', '#f8a283'];
+
+    // С интервалом в секунду присваивает Лого случайные цвета из имеющихся
+    function useRandomColor() {
+        function getRandomInt(max: number) {
+            return Math.floor(Math.random() * max);
+        }
+        setInterval(() => {
+            rootStore.logoColors = {
+                first: colors[getRandomInt(5)],
+                second: colors[getRandomInt(5)],
+                third: colors[getRandomInt(5)],
+                fourth: colors[getRandomInt(5)],
+                fifth: colors[getRandomInt(5)],
+                sixth: colors[getRandomInt(5)]
+            }
+        }, 1000);
+        return
+    }
+
+    useEffect(() => useRandomColor(), []);
+
     return(
         <div className="App">
             <header className="App-header">
                 <Circle width={400} height={400} color={rootStore.circleBackgroundColor}>
                     <div className="App-logo">
-                        <Logo color={rootStore.log1Color} />
-                        <Logo color={rootStore.log2Color} />
-                        <Logo color={rootStore.log3Color} />
+                        <Logo color={rootStore.logoColors.first} />
+                        <Logo color={rootStore.logoColors.second} />
+                        <Logo color={rootStore.logoColors.third} />
                     </div>
                     <div className="App-logo-2">
-                        <Logo color={rootStore.log4Color} />
-                        <Logo color={rootStore.log5Color} />
-                        <Logo color={rootStore.log6Color} />
+                        <Logo color={rootStore.logoColors.fourth} />
+                        <Logo color={rootStore.logoColors.fifth} />
+                        <Logo color={rootStore.logoColors.sixth} />
                     </div>
                 </Circle>
-                <button onClick={() => rootStore.mixColors()}>Mix It!</button>
             </header>
         </div>
     )
--- src/root-store.ts
+++ src/root-store.ts
@@ -2,31 +2,24 @@ import { makeAutoObservable } from "mobx";
 
 class Store {
     circleBackgroundColor: string = '#F2E4EB';
-    log1Color: string = '#D5A6BD';
-    log2Color: string = '#A6D5BE';
-    log3Color: string = '#D5A6BD';
-    log4Color: string = '#A6D5BE';
-    log5Color: string = '#D5A6BD';
-    log6Color: string = '#A6D5BE';
-
-    colors = ['#D5A6BD', '#F2E4EB', '#A6D5BE'];
+    logoColors: {
+        first: string,
+        second: string,
+        third: string,
+        fourth: string,
+        fifth: string,
+        sixth: string
+    } = {
+        first: '#D5A6BD',
+        second: '#A6D5BE',
+        third: '#D5A6BD',
+        fourth: '#A6D5BE',
+        fifth: '#D5A6BD',
+        sixth: '#A6D5BE'
+    }
 
     constructor() {
         makeAutoObservable(this);
-    }
-
-    getRandomInt(max: number) {
-        return Math.floor(Math.random() * max);
-    }
-
-    mixColors() {
-        this.log1Color = this.colors[this.getRandomInt(3)];
-        this.log2Color = this.colors[this.getRandomInt(3)];
-        this.log3Color = this.colors[this.getRandomInt(3)];
-        this.log4Color = this.colors[this.getRandomInt(3)];
-        this.log5Color = this.colors[this.getRandomInt(3)];
-        this.log6Color = this.colors[this.getRandomInt(3)];
-        this.circleBackgroundColor = this.colors[this.getRandomInt(3)];
     }
 
 }
